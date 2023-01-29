bbe4303666e8926c983d44d2aa016d0e4a4dbb8b
Simona Liamo <liamosimona@yahoo.com>
2023-01-29 15:01:41 +0200

    format index.js file

--- HTMLandJSAdvanced/index.js
+++ HTMLandJSAdvanced/index.js
@@ -4,14 +4,12 @@ const button = document.getElementById('element-add-button');
 
 const alertButtonClick = (event) => alert(`Button was clicked which id is: ${event.id}`);
 
-const addElement = () =>  {
-   const text = document.createElement('p');
-   text.innerHTML = 'This is the text';
-   document.body.appendChild(text);
+const addElement = () => {
+    const text = document.createElement('p');
+    text.innerHTML = 'This is the text';
+    document.body.appendChild(text);
 }
 
-buttonId.addEventListener('click', function() {alertButtonClick (this)})
+buttonId.addEventListener('click', function () { alertButtonClick(this) })
 
 button.addEventListener('click', addElement);
-
-// alertButtonClick(buttonId)
